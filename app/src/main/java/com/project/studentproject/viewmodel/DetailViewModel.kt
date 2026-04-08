9480ae1843f9e05d59f504c69259a54ff34d67ea
AriaHansaJunior <ariaoppo2017@gmail.com>
2026-04-08 08:51:03 +0700

    Refactor DetailViewModel to remove hardcoded student data
    The following changes were made:
    - Removed hardcoded student list and manual filtering logic from the `fetch` method.
    - Cleaned up the `fetch` method to rely solely on the Volley `RequestQueue` for data retrieval.

--- app/src/main/java/com/project/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/project/studentproject/viewmodel/DetailViewModel.kt
@@ -43,23 +43,7 @@ class DetailViewModel(application: Application): AndroidViewModel(application) {
                 errorLD.value = true
             }
         )
-
         stringRequest.tag = TAG
         queue?.add(stringRequest)
-
-        val list = arrayListOf(
-            Student("16055","Nonie","1998/03/28","5718444778",
-                "http://dummyimage.com/75x100.jpg/cc0000/ffffff"),
-            Student("13312","Rich","1994/12/14","3925444073",
-                "http://dummyimage.com/75x100.jpg/5fa2dd/ffffff"),
-            Student("11204","Dinny","1994/10/07","6827808747",
-                "http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1")
-        )
-
-        val result = list.find { it.id == id }
-
-        result?.let {
-            studentLD.value = it
-        }
     }
 }
